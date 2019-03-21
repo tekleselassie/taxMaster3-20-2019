382cf830a5b5d0d6c9b3861e1f5d0e29f11b3d6a
CamposDavid415 <47193474+CamposDavid415@users.noreply.github.com>
2019-03-21 00:15:33 -0700

    Version 3
    Version 3

--- js/init.js
+++ js/init.js
@@ -1,4 +1,103 @@
 (function($){
+
+  $(function(){
+
+
+
+    $('.sidenav').sidenav();
+
+
+
+  }); // end of document ready
+
+})(jQuery); // end of jQuery name space
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var elems = document.querySelectorAll('.datepicker');
+
+    var instances = M.Datepicker.init(elems, options);
+
+  });
+
+
+
+  // Or with jQuery
+
+
+
+  $(document).ready(function(){
+
+    $('.datepicker').datepicker();
+
+  });
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var elems = document.querySelectorAll('.parallax');
+
+    var instances = M.Parallax.init(elems, options);
+
+  });
+
+
+
+  // Or with jQuery
+
+
+
+  $(document).ready(function(){
+
+    $('.parallax').parallax();
+
+  });
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+
+      var elems = document.querySelectorAll('.dropdown-trigger');
+
+      var instances = M.Dropdown.init(elems, options);
+
+    });
+
+
+
+    // Or with jQuery
+
+
+
+    $('.dropdown-trigger').dropdown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function($){
   $(function(){
 
     $('.sidenav').sidenav();
